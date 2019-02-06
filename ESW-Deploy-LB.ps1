db74c2a87c1326fe6074e58cb35816ad49bb32c8
William <williamoconnor@me.com>
2019-02-06 01:09:56 +0000

    Push to remote
    Add PowerShell script and arm template for load balancer and availability set

---
+++ ESW-Deploy-LB.ps1
@@ -0,0 +1,79 @@
+param(
+ [Parameter(Mandatory=$True)]
+ [string]
+ $subscriptionId,
+
+ [Parameter(Mandatory=$True)]
+ [string]
+ $deploymentName,
+
+ [string]
+ $templateFilePath = "template.json",
+
+ [string]
+ $parametersFilePath = "parameters.json"
+)
+
+# RG Function
+Function CreateRG {
+
+    #Create or check for existing resource group
+    $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
+    if(!$resourceGroup)
+    {
+        Write-Host "Resource group '$resourceGroupName' does not exist. To create a new resource group, please enter a location.";
+        if(!$resourceGroupLocation) {
+            $resourceGroupLocation = Read-Host "resourceGroupLocation";
+        }
+        Write-Host "Creating resource group '$resourceGroupName' in location '$resourceGroupLocation'";
+        New-AzureRmResourceGroup -Name $resourceGroupName -Location $resourceGroupLocation
+    }
+    else{
+        Write-Host "Using existing resource group '$resourceGroupName'";
+    }
+}
+
+Function RegisterRP {
+    Param(
+        [string]$ResourceProviderNamespace
+    )
+
+    Write-Host "Registering resource provider '$ResourceProviderNamespace'";
+    Register-AzureRmResourceProvider -ProviderNamespace $ResourceProviderNamespace;
+}
+
+#******************************************************************************
+# Script body
+# Execution begins here
+#******************************************************************************
+$resourceGroupName = "ESW-RG"
+$resourceGroupLocation = "North Europe"
+$ErrorActionPreference = "Stop"
+
+# sign in
+Write-Host "Logging in...";
+Login-AzureRmAccount;
+
+# select subscription
+Write-Host "Selecting subscription '$subscriptionId'";
+Select-AzureRmSubscription -SubscriptionID $subscriptionId;
+
+# Register RPs
+$resourceProviders = @("microsoft.compute","microsoft.devtestlab","microsoft.network");
+if($resourceProviders.length) {
+    Write-Host "Registering resource providers"
+    foreach($resourceProvider in $resourceProviders) {
+        RegisterRP($resourceProvider);
+    }
+}
+
+# Create ESW Resource Group
+CreateRG
+
+# Start the deployment
+Write-Host "Starting deployment...";
+if(Test-Path $parametersFilePath) {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath;
+} else {
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $resourceGroupName -TemplateFile $templateFilePath;
+}
